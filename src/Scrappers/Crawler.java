5a22e3c993e4224142450bab03c40b62484e5a7d
peterkim1994 <52155345+peterkim1994@users.noreply.github.com>
2020-10-07 02:42:27 +1300

    scraped upcoming fihgts

--- src/Scrappers/Crawler.java
+++ src/Scrappers/Crawler.java
@@ -6,9 +6,6 @@
 package Scrappers;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,9 +18,9 @@ public class Crawler {
  
     public static void main(String[] args){     
         try {
-              for(int i =1 ; i<=12; i++)
-                  EventCrawler.scrapeEvent(i);
-        //    EventCrawler.scrapeEventPage("http://www.ufcstats.com/event-details/fd4578cac86d75ca", false);
+           //   for(int i =1 ; i<=12; i++)
+           //       EventCrawler.scrapeEvent(i);
+            EventCrawler.scrapeEventPage("http://www.ufcstats.com/event-details/c3c38c86f5ab9b5c", false);
         } catch (IOException ex) {
             Logger.getLogger(Crawler.class.getName()).log(Level.SEVERE, null, ex);
         }
